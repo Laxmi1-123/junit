9e2410bcfe2cdef231f98338de7c1ad2953bb0c5
Laxmi1-123 <laxmi.shelke@cumminscollege.in>
2026-02-06 11:52:32 +0530

    junit

---
+++ JUNIT/add.java
@@ -0,0 +1,10 @@
+package JUNIT;
+
+public class add {
+    public int addnumbers(int x,int y){
+        int sum=0;
+        sum=x+y;
+        System.out.println("Sum is:"+sum);
+        return sum;
+    }
+}
